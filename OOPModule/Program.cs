9d12b3012eb5baf60057a55f2205e8dffd4fa9e4
roman_suyezov <roman_suyezov@epam.com>
2021-11-09 15:13:05 +0600

    cumulative commit with 3rd task

--- OOPModule/Program.cs
+++ OOPModule/Program.cs
@@ -14,13 +14,39 @@ namespace OOPModule
 
             Console.WriteLine("Press corresponding key to enable device(s): Hub (0), Microwave (1), TV (2), Lightbulb (3), Radio (4). Submit final list of device(s) by pressing (e) button.");
 
-            for (int i = 0; i < allowedDevicesAmount; i++)
+            for (int i = 0; i < allowedDevicesAmount; i++) // User is not able to enter more devices than listed in enumerator
             {
-                var device = Console.ReadLine();
-                if (device == "e")
+                var device = "";
+
+                device = Console.ReadLine();
+
+                bool isIntParsed = int.TryParse(device, out int d);
+
+                if (isIntParsed && d > 0) // If parsed - device adds to the list
+                    devicesToEnable.Add(d);
+                else
+                {
+                    if (!isIntParsed && device != "e") // If not parsed and submitting not finished - repeat
+                    {
+                        Console.WriteLine("It is not an integer, try again.");
+                        i--;
+                    }
+
+                    if (d < 0 && device != "e") // If parsed, but negative and submitting not finished - repeat
+                    {
+                        Console.WriteLine("Negative numbers are not allowed, try again.");
+                        i--;
+                    }
+                }
+
+                if (devicesToEnable.Count() == 0 && device == "e") // If user submitted empty device list - repeat
+                {
+                    Console.WriteLine("You have not entered anything, try again.");
+                    i--;
+                }
+
+                if (devicesToEnable.Count() > 0 && device == "e") // If user submitted valid devices list - exit
                     break;
-                int.TryParse(device, out int d);
-                devicesToEnable.Add(d);
             }
 
             Console.WriteLine("Enabled device(s): " + string.Join(", ", new DeviceEnabler().ShowEnabledDevices(devicesToEnable)));
@@ -59,18 +85,33 @@ namespace OOPModule
     {
         public static void Finder()
         {
-            var inputPower = int.Parse(Console.ReadLine());
             var devicePowerList = new DeviceManager().DevicePower;
 
-            var deviceIndex = int.Parse(devicePowerList.Count().ToString());
+            var inputPower = "";
 
-            for (int i = 0; i < deviceIndex; i++)
+            inputPower = Console.ReadLine();
+
+            var parsedPower = int.TryParse(inputPower, out int p);
+
+            if (parsedPower) // Check for integer, if true - continue with execution
             {
-                if (inputPower == devicePowerList[i])
+                int roundedPower = devicePowerList.Min(i => (Math.Abs(p - i), i)).i; // Here I round value to the closest power value to avoid empty search result
+
+                var deviceIndex = int.Parse(devicePowerList.Count().ToString());
+
+                for (int i = 0; i < deviceIndex; i++)
                 {
-                    var device = (DeviceManager.DeviceList)i;
-                    Console.WriteLine("Device with suitable power consumption is " + device.ToString());
+                    if (roundedPower == devicePowerList[i])
+                    {
+                        var device = (DeviceManager.DeviceList)i;
+                        Console.WriteLine("Device with suitable power consumption is " + device.ToString());
+                    }
                 }
+            }
+
+            if (!parsedPower) // If check for integer is failed, user will be asked to enter it again
+            {
+                Console.WriteLine("Only integers are acceptable! Try again.");
             }
         }
     }
