4e6850e08a63fb1bf8226da91a4b7d6d61780423
roman_suyezov <roman_suyezov@epam.com>
2021-11-26 20:23:53 +0600

    intermediate fixes

--- FileSystemModule/Program.cs
+++ FileSystemModule/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace FileSystemModule
 {
@@ -8,7 +9,7 @@ namespace FileSystemModule
     {
         static void Main(string[] args)
         {
-            new TaskOneHandler().TaskHandler();
+            //new TaskOneHandler().TaskHandler();
             new TaskTwoHandler().TaskHandler();
         }
     }
@@ -20,9 +21,9 @@ namespace FileSystemModule
             // There is much better way to get location of the folder "Downloads", via WinAPI, however I decided not to overcomplicate the task
 
             string userFolder = Environment.GetEnvironmentVariable("USERPROFILE");
-            string downloadFolder = Path.Combine(userFolder, "Downloads");
+            string downloadsFolder = Path.Combine(userFolder, "Downloads");
             string fileName = "TextFile.txt";
-            string fullCombinedPath = Path.Combine(downloadFolder, fileName);
+            string fullCombinedPath = Path.Combine(downloadsFolder, fileName);
 
             if (File.Exists(fullCombinedPath))
                 File.Delete(fullCombinedPath);
@@ -66,6 +67,42 @@ namespace FileSystemModule
     {
         public void TaskHandler()
         {
+            string userFolder = Environment.GetEnvironmentVariable("USERPROFILE");
+            string downloadsFolder = Path.Combine(userFolder, "Downloads");
+            List<string> subDirectories = Directory.GetDirectories(downloadsFolder).ToList();
+            List<string> allFiles = new List<string>();
+            string outputFileName = "SearchResults.txt";
+
+            foreach (string f in Directory.GetFiles(downloadsFolder).ToList())
+                allFiles.Add(f);
+
+            foreach (string a in subDirectories)
+            {
+                List<string> newFiles = Directory.GetFiles(a).ToList();
+                foreach (string n in newFiles)
+                    allFiles.Add(n);
+            }
+
+            Console.WriteLine("Enter search request:");
+            string request = Console.ReadLine();
+
+            List<string> searchResults = allFiles.FindAll(r => r.Contains(request));
+            
+            if (searchResults.Count == 0)
+                Console.WriteLine("No matches.");
+            else
+            {
+                foreach (string m in searchResults)
+                    Console.WriteLine(m);
+            }
+
+
+            foreach (string z in allFiles)
+            {
+                var a = new FileInfo(z);
+                Console.WriteLine(z + a.Length);
+
+            }
 
         }
     }
