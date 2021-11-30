9a30d4bba1b87fb7b7565804f035a126f4792fec
roman_suyezov <roman_suyezov@epam.com>
2021-11-30 19:29:20 +0600

    cumulative commit w task 3

--- FileSystemModule/Program.cs
+++ FileSystemModule/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.IO;
 using System.Linq;
 
@@ -10,7 +11,8 @@ namespace FileSystemModule
         static void Main(string[] args)
         {
             //new TaskOneHandler().TaskHandler();
-            new TaskTwoHandler().TaskHandler();
+            //new TaskTwoHandler().TaskHandler();
+            new TaskThreeHandler().TextVersionControl();
         }
     }
 
@@ -83,27 +85,136 @@ namespace FileSystemModule
                     allFiles.Add(n);
             }
 
+            // Search functionality
+
             Console.WriteLine("Enter search request:");
             string request = Console.ReadLine();
-
             List<string> searchResults = allFiles.FindAll(r => r.Contains(request));
-            
             if (searchResults.Count == 0)
                 Console.WriteLine("No matches.");
             else
-            {
                 foreach (string m in searchResults)
                     Console.WriteLine(m);
-            }
 
-
-            foreach (string z in allFiles)
+            Dictionary<string, long> filesWithSize = new Dictionary<string, long>();
+            foreach (string x in allFiles)
             {
-                var a = new FileInfo(z);
-                Console.WriteLine(z + a.Length);
-
+                string file = x;
+                var size = new FileInfo(file).Length;
+                filesWithSize.Add(file, size);
             }
 
+            // Biggest files
+
+            var sortedFiles = from z in filesWithSize orderby z.Value descending select z.Key;
+
+            const int topFilesAmount = 5;
+
+            var biggestFilesNames = sortedFiles.Take(topFilesAmount).ToList();
+
+            for (int i = 0; i < topFilesAmount; i++)
+            {
+                var file = biggestFilesNames[i];
+                var a = new FileInfo(file);
+                Console.WriteLine("Size of " + file + " is " + a.Length);
+            }
+
+            // Average size
+
+            var fileSizes = from y in filesWithSize orderby y.Value descending select y.Value;
+            var convertedFileSizes = fileSizes.ToArray();
+            var averageBytes = convertedFileSizes.Average();
+            var averageMegabytes = (int)averageBytes / 1024;
+
+            Console.WriteLine("Average file size in the selected directory is " + averageMegabytes + " MB");
+
+            // Amount of file names, starting with each alphabet letter
+
+            List<string> fileNamesOnly = new List<string>();
+
+            foreach (string t in allFiles)
+            {
+                string name = t.Replace(downloadsFolder + "\\", "");
+                fileNamesOnly.Add(name);
+            }
+
+            if (subDirectories.Count > 0)
+            {
+                List<string> subDirNames = new List<string>();
+                foreach (string dir in subDirectories)
+                    subDirNames.Add(dir.Replace(downloadsFolder + "\\", ""));
+
+                foreach (string subDir in subDirNames)
+                {
+                    foreach (string x in fileNamesOnly)
+                    {
+                        x.Replace(subDir + "\\", "");
+                    }
+                }
+            }
+
+            Console.WriteLine("");
+        }
+    }
+
+    public class TaskThreeHandler
+    {
+        public void TextVersionControl()
+        {
+            string userFolder = Environment.GetEnvironmentVariable("USERPROFILE");
+            string targetControlFolder = Path.Combine(userFolder, "Downloads", "TextVersionControl");
+            string currentDateTime = DateTime.Now.ToString();
+            string formattedDateTime = currentDateTime.Replace("/", "-").Replace(":", "-");
+            string originalName = "";
+            string versioningFile = "VersionControl";
+            List<string> availableStates = new List<string>();
+            string selectedState = "";
+
+            // Entering selected mode
+            Console.WriteLine("Select app mode - seeking for changes (1) or reverting changes (2)");
+            var mode = Console.ReadLine();
+            if (mode == "1")
+            {
+                Console.WriteLine("Observation mode ON, to exit press (s)");
+                if (Console.ReadLine() != "s")
+                {
+                    List<string> filesUnderControl = new List<string>();
+
+                    foreach (string f in Directory.GetFiles(targetControlFolder).ToList())
+                        filesUnderControl.Add(f);
+
+                    var newDirectory = Directory.CreateDirectory(targetControlFolder + "\\" + formattedDateTime);
+
+                    availableStates.Add(formattedDateTime);
+
+                    if (!File.Exists(targetControlFolder + "\\" + versioningFile))
+                        File.Create(targetControlFolder + "\\" + versioningFile).Dispose();
+
+                    using var fileStreamOne = new FileStream(targetControlFolder + "\\" + versioningFile, FileMode.Append);
+                    using var streamWriterOne = new StreamWriter(fileStreamOne);
+
+                    foreach (string i in availableStates)
+                        streamWriterOne.WriteLine(i);
+                    streamWriterOne.Dispose();
+
+                    foreach (string f in filesUnderControl)
+                    {
+                        originalName = new FileInfo(f).Name;
+                        if (originalName != versioningFile)
+                            File.Copy(targetControlFolder + "\\" + originalName, newDirectory + "\\" + originalName);
+                    }
+
+                    Console.WriteLine("Exit!");
+                }
+            }
+
+            if (mode == "2")
+            {
+                Console.WriteLine("Reverse mode ON");
+                Console.WriteLine("Initial state of files has been fixed" + "\n" + "To start restoration, press (2)");
+            }
+
+            Console.WriteLine("");
         }
     }
 }
