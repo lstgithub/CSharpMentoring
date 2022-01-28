66ea15f64a78aa964232ef6e56cad4d56b2dfc0c
roman_suyezov <roman_suyezov@epam.com>
2022-01-29 00:23:00 +0600

    skeleton added

---
+++ DataAccessLayerModule/Models/Orders.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DataAccessLayerModule.Models
+{
+    class Orders
+    {
+        [Required]
+        public int OrderID { get; set; }
+
+        [Required, StringLength(10)]
+        public string CustomerID { get; set; }
+
+        [Required, StringLength(100)]
+        public string EmployeeID { get; set; }
+
+        [Required, StringLength(100)]
+        public string ShipCity { get; set; }
+    }
+}
---
+++ DataAccessLayerModule/Models/OrdersContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DataAccessLayerModule.Models
+{
+    class OrdersContext : DbContext
+    {
+        public DbSet<Orders> Orders { get; set; }
+    }
+}
